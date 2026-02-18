f7821b112f18a43a7701725a648b3180280a7f82
Hilda Enyioko <enyiokohilda@gmail.com>
2026-02-18 05:03:13 +0100

    added register endpoint

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,10 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     role = models.TextField(choices=ROLE_CHOICES)
     
+    # Add unrequired name fields for user profiles
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    
     # Use Email for Login Authentication inline with JWT authentication
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username'] #username is still a required field
--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from rest_framework import generics
 
+# create register view for user registration
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = []  # Allow anyone to access this view for registration
